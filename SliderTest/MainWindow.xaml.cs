fa24cf745c679d46dab184f8d4eca8e361f06647
Erik.Bauer <166793912+theCodeInMe@users.noreply.github.com>
2024-07-09 10:51:46 +0200

    Projektdateien hinzufügen.

---
+++ SliderTest/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace SliderTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeBackColor(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color hgb = Color.FromArgb((byte)alphaSlide.Value, (byte)redSlide.Value, (byte)blueSlide.Value, (byte)greenSlide.Value);
+            Background = new SolidColorBrush(hgb);
+        }
+    }
+}
